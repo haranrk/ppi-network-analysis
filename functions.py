971dfcb8ce7ee0fbc1f008a5f96240a9164a4086
Haran Rajkumar <haranrajkumar97@gmail.com>
2018-01-08 20:43:31 +0530

    16 Working centrality measures

--- functions.py
+++ functions.py
@@ -1,26 +1,46 @@
 import networkx as nx
-def import_data(org_name=158879):
+
+def import_data(org_name=272635):
 	G=nx.Graph()
 	datafile=open("ppi/%s.ppi" % (org_name))
+	print("Importing PPI data")
 	for line in datafile:
 		g=line.split(" ")
 		if(int(g[15])>700):
 			G.add_edges_from([(g[0],g[1])],weight=int(g[15]))
-			print('%s %s with %s' % (g[0],g[1],g[15]))
+			#print('%s %s with %s' % (g[0],g[1],g[15]))
 
 	ess_file = open('ess/%s.ess' % (org_name))
 	ess_proteins = []
 	for line in ess_file:
 		ess_proteins.append(line.strip()) 
-		print(ess_proteins[-1])
+		#print(ess_proteins[-1])
 
 	return G,ess_proteins
 
+def import_data_with_weights(org_name=272635):
+	G=nx.Graph()
+	datafile=open("ppi/%s.ppi" % (org_name))
+	print("Importing PPI data")
+	for line in datafile:
+		g=line.split(" ")
+		G.add_edges_from([(g[0],g[1])],weight=float(g[15])/1000)
+	return G
+
 #Edge Clustering Coefficient
-def ecc(e):
+def ecc(G,e):
     return G[e[0]][e[1]]['ecc']
 
 def ecc_single(G,e):
     numerator = len(list(nx.common_neighbors(G,e[0],e[1]))) + 1
     denominator = min(G.degree(e[0]), G.degree(e[1]))
     return (numerator / (denominator * 1.0))
+
+#Removes all subgraphs except the largest
+def trim_graph(G):
+	sub_graphs=nx.connected_component_subgraphs(G)
+	main_graph=list(sub_graphs)[0]
+	for s in sub_graphs:
+		if(main_graph.number_of_nodes()<s.number_of_nodes()):
+			main_graph=s
+	return main_graph
--- networkanalysis.py
+++ networkanalysis.py
@@ -7,30 +7,22 @@ from collections import Counter
 from collections import OrderedDict
 import math
 import numpy as np
-import powerlaw #to install in linux use easy_install powerlaw
+#import powerlaw #to install in linux use easy_install powerlaw
 import functions as f    
-
-
-
-#np.seterr(divide='ignore', invalid='ignore')
-
 from optparse import OptionParser
+
 usage="Usage: %prog -f <filename>"
-
 parser = OptionParser(usage)
-
 parser.add_option("-f", "--file", dest="org_name", help="Organism ID")
 (options,args)=parser.parse_args()
-
 
 if 'org_name' not in locals():
 	G,ess_proteins = f.import_data()    
 else:
 	G,ess_proteins = f.import_data(org_name)
 
-
 ####2.ANALYZING THE NETWORK####
-print("ANALYZING NETWORK")
+print("Analysing Network")
 centrality_measures = {}
 
 print("1. Degree Centrality")
@@ -58,9 +50,8 @@ subc=nx.subgraph_centrality(G)
 centrality_measures["Subgraph"]=subc
 
 print("7. Information centrality")
-#inc=nx.current_flow_closeness_centrality(G)
-#centrality_measures["Information Centrality"]=inc
-#remove dced comp
+inc=nx.current_flow_closeness_centrality(f.trim_graph(G))
+centrality_measures["Information Centrality"]=inc
 
 print("8. Clique Number")
 cliq={}
@@ -69,11 +60,20 @@ for i in G.nodes():
 centrality_measures["Clique Number"]=cliq
 
 print("9. Edge clustering coefficient")
-for e in G.edges():
-    G[e[0]][e[1]]['ecc'] = f.ecc_single(G,e)
 edge_clus_coeff={}
-for i in G.nodes():
-    edge_clus_coeff[i]=sum(map(f.ecc,G.edges(i)))
+for n in G.nodes:
+	edge_clus_coeff[n]=0
+	for e in G.edges(n):
+		num=len(list(nx.common_neighbors(G,e[0],e[1])))
+		den=(min(G.degree(e[0]),G.degree(e[1]))-1)
+		if den==0:
+			den=1
+		edge_clus_coeff[n]+=num/den
+# for e in G.edges():
+#     G[e[0]][e[1]]['ecc'] = f.ecc_single(G,e)
+
+# for i in G.nodes():
+#     edge_clus_coeff[i]=sum(map(f.ecc(G,e),G.edges(i)))
 centrality_measures['Edge Clustering Coefficient']=edge_clus_coeff
 
 print("10. Page Rank")
@@ -81,24 +81,30 @@ pg=nx.pagerank(G)
 centrality_measures['Page Rank']=pg
 
 print("11. Random Walk Betweenness Centrality")
-#rwbc=nx.current_flow_betweenness_centrality(G)
-#centrality_measures["Random Walk Betweenness Centrality"]=rwbc
+rwbc=nx.current_flow_betweenness_centrality(f.trim_graph(G))
+centrality_measures["Random Walk Betweenness Centrality"]=rwbc
 
 print("12. Load Centrality")
 load_cent=nx.load_centrality(G)
 centrality_measures["Load Centrality"]=load_cent
 
 print("13. Communicability Betweenness")
-#communicability_betweenness=nx.communicability_betweenness_centrality(G)
-#centrality_measures["Communicability Betweenness"]=communicability_betweenness
+communicability_betweenness=nx.communicability_betweenness_centrality(f.trim_graph(G))
+centrality_measures["Communicability Betweenness"]=communicability_betweenness
 
 print("14. Harmonic Centrality")
 harmonic_cent=nx.harmonic_centrality(G)
 centrality_measures["Harmonic Centrality"]=harmonic_cent
-
+	
 print("15. Reaching Centrality")
-#reach_cent = nx.local_reaching_centrality(G)
-#centrality_measures["Reaching Centrality"]=reach_cent
+reach_cent={}
+for node in G.nodes:
+	reach_cent[node] = nx.local_reaching_centrality(G,node)
+centrality_measures["Reaching Centrality"]=reach_cent
+
+print("16. Katz Centrality")
+katz_cent=nx.katz_centrality(G)
+centrality_measures["Katz Centrality"]
 
 for name, values in centrality_measures.items():
 	ess_protein_measures = []
